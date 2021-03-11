3a88208ef44f6afc000cad9f7e84cf3478d57717
chit_san <chitsanchitsan1@gmail.com>
2021-03-11 15:26:20 +0630

    initial commit

---
+++ src/TinderCards.js
@@ -0,0 +1,51 @@
+import React, { useState,useEffect } from "react";
+import "./TinderCards.css";
+import TinderCard from "react-tinder-card";
+import { SwipeableDrawer } from "@material-ui/core";
+import axios from "./axios";
+function TinderCards() {
+  const [people, setPeople] = useState([]);
+  console.log(people);
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing" + nameToDelete);
+    // setLastDirection(direction);
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get("/tinder/cards");
+      console.log(req.data);
+      setPeople(req.data);
+    }
+    fetchData();
+  }, []);
+  const outOfFrame = (name) => {
+    console.log(name + "left the screen");
+  };
+  return (
+    <div className="tinderCards">
+      <div className="tinderCards__cardContainer">
+        {people.map((person) => {
+          return (
+            <TinderCard
+              className="swipe"
+              key={person.name}
+              preventSwipe={("up", "down")}
+              onSwipe={(dir) => swiped(dir, person.name)}
+              onCardLeftScreen={() => outOfFrame(person.name)}
+            >
+              <div
+                style={{ backgroundImage: `url(${person.imgUrl})` }}
+                className="card"
+              >
+                <h3>{person.name}</h3>
+              </div>
+            </TinderCard>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default TinderCards;
